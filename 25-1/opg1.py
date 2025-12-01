a15bb88649d7f60fd61bae7b682bb921a71ecdf5
Sebe112 <92109053+Sebe112@users.noreply.github.com>
2025-12-01 12:46:36 +0100

    first commit

---
+++ 25-1/opg1.py
@@ -0,0 +1,25 @@
+import re
+
+count = 0
+dial = 50
+
+with open("Data.txt", "r") as file:
+    for line in file:
+        L = re.split("L", line)
+        R = re.split("R", line)
+        if re.search("L", line):
+            for x in range(int(L[1])):
+                dial -= 1 
+                if dial == -1:
+                    dial = 99
+        else:
+            for x in range(int(R[1])):
+                dial += 1
+                if dial == 100:
+                    dial = 0
+        if dial == 0:
+            count += 1
+
+print(count)
+        
+
---
+++ 25-1/opg2.py
@@ -0,0 +1,28 @@
+import re
+import time
+
+count = 0
+dial = 50
+
+with open("Data.txt", "r") as file:
+    for line in file:
+        L = re.split("L", line)
+        R = re.split("R", line)
+        if re.search("L", line):
+            for x in range(int(L[1])):
+                dial -= 1 
+                if dial == 0:
+                    count += 1
+                if dial == -1:
+                    dial = 99
+        else:
+            for x in range(int(R[1])):
+                dial += 1
+                if dial == 100:
+                    count += 1
+                    dial = 0
+
+
+print(count)
+        
+
