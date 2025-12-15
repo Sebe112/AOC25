9de6aba2e3d124ccd0339037befd7cc3beba6e2c
Sebe112 <92109053+Sebe112@users.noreply.github.com>
2025-12-15 10:37:54 +0100

    gg

--- 25-2/opg3.py
+++ 25-2/opg3.py
@@ -1,8 +1,24 @@
 import re
+i = 0
+a = 0
+b = 0
+res = 0
 
 with open("Data.txt", "r") as file:
     for line in file:
         split = re.split(r'[,-]', line)
-        
+
         for x in split:
-            print([int(x)])
+            if i % 2 == 0:
+                a = int(x)
+            else:
+                b = int(x)
+                for y in range(a, b + 1):
+                    first_half  = str(y)[:len(str(y))//2]
+                    second_half = str(y)[len(str(y))//2:]
+
+                    if first_half == second_half:
+                        res += y
+            i +=1
+
+print(res)
